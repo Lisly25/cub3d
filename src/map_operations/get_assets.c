8fce0124fd7e39f470f3e836bca99d825044abe6
Szabina Korbai <skorbai@student.hive.fi>
2024-05-01 10:27:52 +0300

    Started implementing the main skeleton of asset extraction from .cub file

--- src/map_operations/get_assets.c
+++ src/map_operations/get_assets.c
@@ -1,18 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate_map.c                                     :+:      :+:    :+:   */
+/*   get_assets.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:46:21 by skorbai           #+#    #+#             */
-/*   Updated: 2024/04/29 14:47:18 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/05/01 09:52:56 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void	validate_map(t_vector *map)
+void	get_texture_info(t_vector *map, t_assets *assets)
 {
-	
+	extract_texture_path("NO", assets, map);
+	extract_texture_path("SO", assets, map);
+	extract_texture_path("EA", assets, map);
+	extract_texture_path("WE", assets, map);
+	extract_rgb("F", assets, map);
+	extract_rgb("C", assets, map);
+}
+
+t_assets	*get_assets(t_vector *map)
+{
+	t_assets	*assets;
+
+	assets = (t_assets *)malloc(sizeof(t_assets));
+	if (assets == NULL)
+		map_validation_error("Malloc failure", map, NULL);
+	get_texture_info(map, assets);
 }
