d1fb325bec16e5177180ec69a41805df4077a64a
Szabina Korbai <skorbai@student.hive.fi>
2024-05-01 10:49:39 +0300

    Confirmed that extract_array() works as intended, and refactored it

--- src/vector_decrease.c
+++ src/vector_decrease.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 09:59:22 by skorbai           #+#    #+#             */
-/*   Updated: 2024/05/01 10:22:10 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/05/01 10:47:09 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 //this is just for testing - delete before submission
 void	print_vector_content(t_vector *vector)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	if (vector == NULL)
@@ -25,26 +25,18 @@ void	print_vector_content(t_vector *vector)
 	ft_printf("Vector content:\n");
 	while (i < vector->used_nodes)
 	{
-		ft_printf("%s\n", vector->text[i]);
+		ft_printf("%s", vector->text[i]);
 		i++;
 	}
 }
 
-char	*extract_array(t_vector *vector, int array_index)
+static void	rearrange_vector(t_vector *vector, size_t index, char **new_text)
 {
-	char	**new_text;
-	int		i;
-	int		j;
-	char	*extracted_array;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (array_index >= vector->used_nodes)
-		return (NULL);
-	new_text = (char **)malloc(sizeof(char *) * (vector->max_nodes - 1));
-	if (new_text == NULL)
-		return (NULL);
-	extracted_array = vector->text[array_index];
-	while (i < array_index)
+	while (i < index)
 	{
 		new_text[i] = vector->text[i];
 		i++;
@@ -60,4 +52,19 @@ char	*extract_array(t_vector *vector, int array_index)
 	vector->text = new_text;
 	vector->used_nodes = vector->used_nodes - 1;
 	vector->max_nodes = vector->max_nodes - 1;
+}
+
+char	*extract_array(t_vector *vector, size_t array_index)
+{
+	char	**new_text;
+	char	*extracted_array;
+
+	if (array_index >= vector->used_nodes)
+		return (NULL);
+	new_text = (char **)malloc(sizeof(char *) * (vector->max_nodes - 1));
+	if (new_text == NULL)
+		return (NULL);
+	extracted_array = vector->text[array_index];
+	rearrange_vector(vector, array_index, new_text);
+	return (extracted_array);
 }
