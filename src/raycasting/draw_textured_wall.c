e358d7ffaca5fe2b5f06626a5a2a877b1cbf3b20
Finn Shields <fshields@c1r4p3.hive.fi>
2024-05-10 11:40:45 +0300

    now works with textures of any size

--- src/raycasting/draw_textured_wall.c
+++ src/raycasting/draw_textured_wall.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_textured_wall.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: fshields <fshields@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:43:02 by skorbai           #+#    #+#             */
-/*   Updated: 2024/05/10 11:31:40 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/05/10 11:39:19 by fshields         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static double	get_wall_x(int side, t_data *data, t_ray *ray)
 	return (wall_x);
 }
 
-static int	get_texture_x(int side, t_data *data)
+static int	get_texture_x(int side, t_data *data, int tex_w)
 {
 	int		texture_x;
 	double	wall_x;
@@ -32,15 +32,15 @@ static int	get_texture_x(int side, t_data *data)
 
 	ray = data->ray;
 	wall_x = get_wall_x(side, data, ray);
-	texture_x = (int)(wall_x * ((double) TEXTURE_WIDTH));
+	texture_x = (int)(wall_x * ((double) tex_w));
 	if (side == 0 && (ray->ray_direction_x > 0))
-		texture_x = TEXTURE_WIDTH - texture_x - 1;
+		texture_x = tex_w - texture_x - 1;
 	if (side == 1 && (ray->ray_direction_y < 0))
-		texture_x = TEXTURE_WIDTH - texture_x - 1;
+		texture_x = tex_w - texture_x - 1;
 	return (texture_x);
 }
 
-uint32_t	get_element_by_coordinate(int x, int y, uint32_t *arr)
+uint32_t	get_element_by_coordinate(int x, int y, uint32_t *arr, int tex_w)
 {
 	uint32_t	colour;
 	int			index;
@@ -48,7 +48,7 @@ uint32_t	get_element_by_coordinate(int x, int y, uint32_t *arr)
 	if (y == 0)
 		index = x;
 	else
-		index = (y * TEXTURE_WIDTH) + x;
+		index = (y * tex_w) + x;
 	colour =  arr[index];
 	return (colour);
 }
@@ -68,15 +68,15 @@ void	draw_textured_wall_section(int draw_start, int draw_end, int x, t_data *dat
 	ray = data->ray;
 	assets = data->assets;
 	texture = assets->north;
-	step = 1.0 * TEXTURE_HEIGHT / ray->line_height;
-	texture_x = get_texture_x(side, data);
+	step = 1.0 * texture->height / ray->line_height;
+	texture_x = get_texture_x(side, data, texture->width);
 	texture_position = (draw_start - SCREEN_HEIGHT / 2 + ray->line_height / 2) * step;
 	y = draw_start;
 	while (y < draw_end)
 	{
-		texture_y = (int)texture_position & (TEXTURE_HEIGHT - 1);
+		texture_y = (int)texture_position & (texture->height - 1);
 		texture_position += step;
-		color = get_element_by_coordinate(texture_x, texture_y, (uint32_t *)texture->pixels);
+		color = get_element_by_coordinate(texture_x, texture_y, (uint32_t *)texture->pixels, texture->width);
 		mlx_put_pixel(data->img, x, y, color);
 		y++;
 	}
