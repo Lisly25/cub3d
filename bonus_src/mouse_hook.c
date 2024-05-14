7f2efbebc10b60f8ec1fa86aa0fda892ed72c9f8
Finn Shields <fshields@c1r4p3.hive.fi>
2024-05-14 11:24:25 +0300

    mouse issue fixed, makefile updated

--- bonus_src/mouse_hook.c
+++ bonus_src/mouse_hook.c
@@ -6,18 +6,23 @@
 /*   By: fshields <fshields@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:57:49 by fshields          #+#    #+#             */
-/*   Updated: 2024/05/14 09:37:13 by fshields         ###   ########.fr       */
+/*   Updated: 2024/05/14 11:20:50 by fshields         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static void	update_view_mouse(t_data *data, int mouse_x, int old_x)
+static void	update_view_mouse(t_data *data, int mouse_x, int old_x, int *start)
 {
 	double	change;
 	double	old_dir_x;
 	double	old_plane_x;
 
+	if (*start == 0)
+	{
+		*start = 1;
+		return ;
+	}
 	change = 0.005 * abs(old_x - mouse_x);
 	if (mouse_x < old_x)
 		change *= -1;
@@ -36,11 +41,12 @@ void	mouse_hook(void *param)
 	static int32_t	x;
 	static int32_t	y;
 	int32_t			old_x;
+	static int		start;
 
 	old_x = x;
 	data = param;
 	mlx_get_mouse_pos(data->window, &x, &y);
 	if (x >= 0 && x <= SCREEN_WIDTH && x != old_x && y >= 0 \
 	&& y <= SCREEN_HEIGHT)
-		update_view_mouse(data, x, old_x);
+		update_view_mouse(data, x, old_x, &start);
 }
